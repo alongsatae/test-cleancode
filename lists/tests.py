0f7566dac31344e082a46c177b1961279a31606c
rumblebit <seoulmilk1a@naver.com>
2015-06-02 22:31:54 +0900

    첫 단위 테스트와 url맵핑 그리고 임시 뷰

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -19,3 +20,14 @@ class HomePageTest(TestCase):
 		self.assertTrue(response.content.startswith(b'<html>'))
 		self.assertIn(b'<title>To-Do lists</title>', response.content)
 		self.assertTrue(response.content.endswith(b'</html>'))
+
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = '신규 작업 아이템'
+
+		response = home_page(request)
+
+		self.assertIn('신규 작업 아이템', response.content.decode())
+		expected_html = render_to_string('home.html', {'new_item_text': '신규 작업 아이템'})
+		self.assertEqual(response.content.decode(), expected_html)
