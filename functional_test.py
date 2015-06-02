322ad4533e8317de6736a5af75fdcaa14499f19c
rumblebit <seoulmilk1a@naver.com>
2015-06-02 22:12:05 +0900

    템플릿을 이용한 메인 페이지 HTML 생성

--- functional_test.py
+++ functional_test.py
@@ -18,15 +18,16 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn('To-Do', header_text)
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(inputbox.get_attribute('placeholder'), '직접 아이템 입력')
+		self.assertEqual(inputbox.get_attribute('placeholder'), '작업 아이템 입력')
 
 		inputbox.send_keys('공작깃털 사기')
 
 		inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_element_by_id('id_list+_table')
-		rows = table.find_element_by_tag_name('tr')
-		self.assertTrue(any(row.text == '1: 공작깃털 사기' for row in  rows),)
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(any(row.text == '1: 공작깃털 사기' for row in  rows),
+			"신규 작업이 테이블에 표시되지 않는다")
 
 		self.fail('Finish the test!')
 
