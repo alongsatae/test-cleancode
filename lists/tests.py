baef720e9bd51f506d91d5fcb1b1ffc8efeb3ee3
rumblebit <seoulmilk1a@naver.com>
2015-06-03 22:54:18 +0900

    두개 폰을 위한 URL 적용, 신규 URL인 lists/new 를 생성

--- lists/tests.py
+++ lists/tests.py
@@ -21,51 +21,11 @@ class HomePageTest(TestCase):
 		self.assertIn(b'<title>To-Do lists</title>', response.content)
 		self.assertTrue(response.content.endswith(b'</html>'))
 
-	def test_home_page_can_save_a_POST_request(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = '신규 작업 아이템'
-
-		response = home_page(request)
-
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, '신규 작업 아이템')
-
-#		self.assertEqual(response.status_code, 302)
-#		self.assertEqual(response['location'], '/')
-
-		#응답이 리디렉션을 하기 때문에, 어설션 제거
-#		self.assertIn('신규 작업 아이템', response.content.decode())
-#		expected_html = render_to_string('home.html', {'new_item_text': '신규 작업 아이템'})
-#		self.assertEqual(response.content.decode(), expected_html)
-
-
-	def test_home_page_redirects_after_POST(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = '신규 작업 아이템'
-
-		response = home_page(request)
-
-		self.assertEqual(response.status_code, 302)
-		#self.assertEqual(response['location'], '/')
-		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-	# def test_home_page_displays_all_list_items(self):
-	# 	Item.objects.create(text = 'itemey 1')
-	# 	Item.objects.create(text = 'itemey 2')
-
+	
+	# def test_home_page_only_saves_items_when_necessary(self):
 	# 	request = HttpRequest()
-	# 	response = home_page(request)
-
-	# 	self.assertIn('itemey 1', response.content.decode())
-	# 	self.assertIn('itemey 2', response.content.decode())
-
-	def test_home_page_only_saves_items_when_necessary(self):
-		request = HttpRequest()
-		home_page(request)
-		self.assertEqual(Item.objects.count(), 0)
+	# 	home_page(request)
+	# 	self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
 	def test_saving_and_retrieving_items(self):
@@ -89,7 +49,7 @@ class ListViewTest(TestCase):
 	def test_uses_list_templatee(self):
 		response=self.client.get('/lists/the-only-list-in-the-world/')
 		self.assertTemplateUsed(response, 'list.html')
-		
+
 	def test_displays_all_items(self):
 		Item.objects.create(text = 'itemey 1')
 		Item.objects.create(text = 'itemey 2')
@@ -98,3 +58,49 @@ class ListViewTest(TestCase):
 
 		self.assertContains(response, 'itemey 1')
 		self.assertContains(response, 'itemey 2')
+
+class NewListTest(TestCase):
+	def test_saving_a_POST_request(self):
+		# request = HttpRequest()
+		# request.method = 'POST'
+		# request.POST['item_text'] = '신규 작업 아이템'
+
+		# response = home_page(request)
+		self.client.post('/lists/new', data = {'item_text': '신규 작업 아이템'})
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, '신규 작업 아이템')
+
+#		self.assertEqual(response.status_code, 302)
+#		self.assertEqual(response['location'], '/')
+
+		#응답이 리디렉션을 하기 때문에, 어설션 제거
+#		self.assertIn('신규 작업 아이템', response.content.decode())
+#		expected_html = render_to_string('home.html', {'new_item_text': '신규 작업 아이템'})
+#		self.assertEqual(response.content.decode(), expected_html)
+
+
+	def test_redirects_after_POST(self):
+		# request = HttpRequest()
+		# request.method = 'POST'
+		# request.POST['item_text'] = '신규 작업 아이템'
+
+		# response = home_page(request)
+		response = self.client.post('/lists/new', data = {'item_text': '신규 작업 아이템'})
+		
+		#self.assertEqual(response.status_code, 302)
+		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+		# #self.assertEqual(response['location'], '/')
+		#self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+
+	# def test_home_page_displays_all_list_items(self):
+	# 	Item.objects.create(text = 'itemey 1')
+	# 	Item.objects.create(text = 'itemey 2')
+
+	# 	request = HttpRequest()
+	# 	response = home_page(request)
+
+	# 	self.assertIn('itemey 1', response.content.decode())
+	# 	self.assertIn('itemey 2', response.content.decode())
+ 
--- lists/views.py
+++ lists/views.py
@@ -15,10 +15,10 @@ def home_page(request):
 #	return render(request, 'home.html', {'new_item_text': request.POST.get('item_text',''),
 #		})
 
-	if request.method == 'POST':
-		new_item_text = request.POST['item_text']
-		Item.objects.create(text=new_item_text)	#save() 호출이 필요없는 축약 명령
-		return redirect('/lists/the-only-list-in-the-world/')
+	# if request.method == 'POST':
+	# 	new_item_text = request.POST['item_text']
+	# 	Item.objects.create(text=new_item_text)	#save() 호출이 필요없는 축약 명령
+	# 	return redirect('/lists/the-only-list-in-the-world/')
 	#else:
 	#	new_item_text = ''
 
@@ -29,3 +29,7 @@ def home_page(request):
 def view_list(request):
 	items = Item.objects.all()
 	return render(request, 'list.html', {'items':items})
+
+def new_list(request):
+	Item.objects.create(text = request.POST['item_text'])
+	return redirect('/lists/the-only-list-in-the-world/')
